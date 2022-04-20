02fa44c782532b492f1a66017adda8954c758a1b
lovachittumuri <lova.anand@gmail.com>
2022-04-20 21:30:50 +0530

    @Lova Chittumuri - Rest API Implementation Work Started

---
+++ RestAPIImplementApplication/src/main/java/com/demo/RestApiImplementApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiImplementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiImplementApplication.class, args);
+	}
+
+}
